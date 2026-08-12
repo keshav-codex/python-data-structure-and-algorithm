128d51e1b020e25066aea8ced68593a350ca5fb9
keshav-codex <kjkeshav0698@gmail.com>
2026-08-12 15:33:21 +0530

    Traversal

---
+++ 02_Data_Structure/06_Linked_List/03_Circular_singly_linked_list/03_traversal.py
@@ -0,0 +1,24 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+node1 = Node(10)
+node2 = Node(20)
+node3 = Node(30)
+
+node1.next = node2
+node2.next = node3
+node3.next = node1
+
+head = node1
+
+current = head
+
+while True:
+    print(current.data)
+    current = current.next
+
+    if current == head:
+        break
