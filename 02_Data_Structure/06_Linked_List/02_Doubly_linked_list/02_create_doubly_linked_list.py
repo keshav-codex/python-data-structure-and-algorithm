6359dc67c216b1c7ced3a07a49219721ba33a01a
keshav-codex <kjkeshav0698@gmail.com>
2026-08-12 15:26:49 +0530

    Doubly linked list creation

---
+++ 02_Data_Structure/06_Linked_List/02_Doubly_linked_list/02_create_doubly_linked_list.py
@@ -0,0 +1,22 @@
+class Node:
+    def __init__(self, data):
+        self.prev = None
+        self.data = data
+        self.next = None
+
+
+node1 = Node(10)
+node2 = Node(20)
+node3 = Node(30)
+
+node1.next = node2
+node2.prev = node1
+
+node2.next = node3
+node3.prev = node2
+
+head = node1
+
+print(head.data)
+print(head.next.data)
+print(head.next.next.data)
