53a5882af786cce9e6627b85fe085db1ab70c694
keshav-codex <kjkeshav0698@gmail.com>
2026-08-12 15:36:24 +0530

    Circular linked list counts nodes

---
+++ 02_Data_Structure/06_Linked_List/03_Circular_singly_linked_list/11_count_nodes.py
@@ -0,0 +1,29 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+node1 = Node(10)
+node2 = Node(20)
+node3 = Node(30)
+node4 = Node(40)
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node1
+
+head = node1
+
+count = 0
+current = head
+
+while True:
+    count += 1
+    current = current.next
+
+    if current == head:
+        break
+
+print("Total Nodes:", count)
