c8aaa59e06e0579c27a14e40be9fb16b4d910fc8
keshav-codex <kjkeshav0698@gmail.com>
2026-08-12 15:30:06 +0530

    Doubly linked list search

---
+++ 02_Data_Structure/06_Linked_List/02_Doubly_linked_list/11_search_element.py
@@ -0,0 +1,35 @@
+class Node:
+    def __init__(self, data):
+        self.prev = None
+        self.data = data
+        self.next = None
+
+
+node1 = Node(10)
+node2 = Node(20)
+node3 = Node(30)
+
+node1.next = node2
+node2.prev = node1
+
+node2.next = node3
+node3.prev = node2
+
+head = node1
+
+target = 20
+
+current = head
+found = False
+
+while current is not None:
+    if current.data == target:
+        found = True
+        break
+
+    current = current.next
+
+if found:
+    print("Element Found")
+else:
+    print("Element Not Found")
