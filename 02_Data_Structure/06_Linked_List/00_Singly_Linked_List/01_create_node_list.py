f7f8f47bec5dec5302c96729390bf77895eb76a8
keshav-codex <kjkeshav0698@gmail.com>
2026-08-12 15:18:24 +0530

    Singly linked list creation

---
+++ 02_Data_Structure/06_Linked_List/00_Singly_Linked_List/01_create_node_list.py
@@ -0,0 +1,25 @@
+# create nodes and connect those into linked list
+
+# Crearting a singly linked list node bluprint
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+# Creting nodes or node objects
+node1 = Node(10)
+node2 = Node(20)
+node3 = Node(30)
+node4 = Node(40)
+
+# creting linked list
+
+# creating head
+head = node1
+
+# linking node to next value
+node1.next = node2
+node2.next = node3
+node3.next = node4
+
+# Node 4 is last and by default .next point to None.
