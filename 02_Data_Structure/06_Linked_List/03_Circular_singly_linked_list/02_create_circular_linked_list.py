404ea6168a5203bf7ea9a6c2a3a539ebef39c972
keshav-codex <kjkeshav0698@gmail.com>
2026-08-12 15:32:49 +0530

    Creating circular singly linked list

---
+++ 02_Data_Structure/06_Linked_List/03_Circular_singly_linked_list/02_create_circular_linked_list.py
@@ -0,0 +1,20 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+node1 = Node(10)
+node2 = Node(20)
+node3 = Node(30)
+
+node1.next = node2
+node2.next = node3
+node3.next = node1
+
+head = node1
+
+print(head.data)
+print(head.next.data)
+print(head.next.next.data)
+print(head.next.next.next.data)
