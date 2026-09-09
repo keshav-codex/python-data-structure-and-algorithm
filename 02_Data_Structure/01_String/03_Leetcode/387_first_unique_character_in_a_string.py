2b1be0725ddf13dd19fec58fa9070fd8a3ad3cce
keshav-codex <kjkeshav0698@gmail.com>
2026-09-09 19:01:18 +0530

    leetcode 387

---
+++ 02_Data_Structure/01_String/03_Leetcode/387_first_unique_character_in_a_string.py
@@ -0,0 +1,15 @@
+# Given a string s, find the first non-repeating character in it and return its index. 
+# If it does not exist, return -1.
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        char_count = {}
+
+        for char in s:
+            char_count[char] = char_count.get(char,0) +1
+
+        for i in range(len(s)):
+            if char_count[s[i]] == 1:
+                return i
+
+        return -1
